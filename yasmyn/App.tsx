9d7b5638bfddbdfbed4ae4fdb160d5265e13f431
Piotr Szczepaniak <pszczepaniak@qualtrics.com>
2025-06-16 12:21:09 +0200

    rocky balboa

--- yasmyn/App.tsx
+++ yasmyn/App.tsx
@@ -15,7 +15,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Register">
+        <Stack.Navigator initialRouteName="Login">
           <Stack.Screen name="Register" component={RegisterScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Photos" component={PhotosScreen} />
